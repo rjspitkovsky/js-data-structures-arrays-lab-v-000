a6f0372f3bd2627a7c3245bb6ff2e923f52ab012
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-02 17:55:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,8 @@ function destructivelyRemoveLastDriver() {
 function destructivelyRemoveFirstDriver() {
   drivers.shift()
 }
+
+function appendDriver(name) {
+  const newArray = [...drivers, name]
+  return newArray
+}
